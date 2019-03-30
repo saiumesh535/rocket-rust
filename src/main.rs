fafbe67e9f61f20140ac395ca1b11d7575a27eaa
saiumesh535 <d.sai535@gmail.com>
2019-03-30 15:35:32 +0530

    added json code

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@
 #[macro_use]
 extern crate rocket;
 
+#[macro_use]
+extern crate serde_derive;
+
 mod index;
 mod error_handlers;
+mod json_example;
 
 use error_handlers::handlers;
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index::index])
+        .mount("/", routes![index::index, json_example::json])
         .register(catchers![handlers::not_found])
         .launch();
 }
