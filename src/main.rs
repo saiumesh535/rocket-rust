cb6e2bc1e53d6cba3ebc53333576de631e74d74f
saiumesh535 <d.sai535@gmail.com>
2019-03-30 23:23:48 +0530

    first cut for database connection 😍🔥🔥

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,23 @@ mod json_example;
 mod form_example;
 mod auth;
 mod state;
+mod database;
 
 use error_handlers::handlers;
 use auth::login;
 use state::test_state::MyConfig;
+use database::postgres_sql;
+use state::pg_state::PgState;
+use std::{sync::Mutex};
 
 fn main() {
 
     let lol = MyConfig {
         user_val: String::from("Hey!!")
+    };
+
+    let pg_conn = PgState {
+        connection: Mutex::new(postgres_sql::connect_pg())
     };
 
     rocket::ignite()
@@ -32,6 +40,7 @@ fn main() {
             login::login_handler
         ])
         .manage(lol)
+        .manage(pg_conn)
         .register(catchers![handlers::not_found])
         .launch();
 }
